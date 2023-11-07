1ec7e10413dad33124088abc77e46dcb116b484c
Silas Eduardo <silasn1@live.com>
2023-11-07 17:55:14 -0300

    att

--- api_login.py
+++ api_login.py
@@ -1,18 +1,65 @@
-import gspread
-from google.oauth2.service_account import Credentials
+import os.path
 
-# Configurar as credenciais do OAuth2
-credentials = Credentials.from_service_account_file('client_secret_775679487471-fsj4kf2b8rqn1f9nl7nhra47lc14mu47.apps.googleusercontent.com.json', scopes=['https://www.googleapis.com/auth/spreadsheets'])
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# Autenticar usando as credenciais
-gc = gspread.authorize(credentials)
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-# Abra a planilha pelo seu ID ou URL
-spreadsheet = gc.open_by_key('https://docs.google.com/spreadsheets/d/1cwDPhDXBbnIYqMjzxfGkPMOPQnzX2H0Tr1YtuVKJiQc/edit#gid=0')
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+SAMPLE_RANGE_NAME = "Class Data!A2:E"
 
-# Selecione a planilha desejada
-worksheet = spreadsheet.get_worksheet(0)  # 0 representa a primeira planilha na lista
 
-# Alimente os dados na planilha
-data_to_insert = ["Dado 1", "Dado 2", "Dado 3"]
-worksheet.append_row(data_to_insert)
+def main():
+  """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = (
+        sheet.values()
+        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        .execute()
+    )
+    values = result.get("values", [])
+
+    if not values:
+      print("No data found.")
+      return
+
+    print("Name, Major:")
+    for row in values:
+      # Print columns A and E, which correspond to indices 0 and 4.
+      print(f"{row[0]}, {row[4]}")
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  main()
